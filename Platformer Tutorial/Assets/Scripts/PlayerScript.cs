6e4bbe1312085a54482f319dff9a93267536e068
HeatherParrett <heather@gmail.com>
2019-10-27 22:24:13 -0400

    adding platform challenge

--- Platformer Tutorial/Assets/Scripts/PlayerScript.cs
+++ Platformer Tutorial/Assets/Scripts/PlayerScript.cs
@@ -9,19 +9,37 @@ public class PlayerScript : MonoBehaviour
 
     public float speed;
 
-    public Text score;
+    public Text countText;
+    public Text winText;
+    public Text lifeText;
+    public Text loseText;
 
-    private int scoreValue = 0;
+    public AudioSource winSource;
+
+    private int count;
+    private int life;
+
+    Animator anim;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
+        winSource = GetComponent<AudioSource>();
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        count = 0;
+        life = 3;
+        winText.text = "";
+        loseText.text = "";
+        SetCountText();
+        SetLifeText();
+        anim = GetCompon
     }
 
     private void Update()
     {
+
         if (Input.GetKey("escape"))
         {
             Application.Quit();
@@ -31,6 +49,7 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+
         float hozMovement = Input.GetAxis("Horizontal");
         float vertMovement = Input.GetAxis("Vertical");
         rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
@@ -40,14 +59,25 @@ public class PlayerScript : MonoBehaviour
     {
         if (collision.collider.tag == "Coin")
         {
-            scoreValue += 1;
-            score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+
+            count = count + 1;
+            SetCountText();
         }
+
+        if (collision.collider.tag == "Enemy")
+        {
+            Destroy(collision.collider.gameObject);
+            life = life - 1;
+            SetLifeText();
+            SetCountText();
+        }
+
 
     }
 
-    private void OnCollisionStay2D(Collision2D collision)
+
+private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.collider.tag == "Ground")
         {
@@ -55,6 +85,36 @@ public class PlayerScript : MonoBehaviour
             {
                 rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
             }
+        }
+    }
+
+    void SetLifeText()
+    {
+        lifeText.text = "<b> Lives: </b>" + life.ToString();
+
+        if (life <= 0)
+        {
+            loseText.text = "You lose! Game created by Heather Parrett";
+            rd2d.constraints = RigidbodyConstraints2D.FreezeAll;
+        }
+
+    }
+
+    void SetCountText()
+    {
+        countText.text = "<b>Count:</b> " + count.ToString();
+
+        if (count >= 8)
+        {
+            winText.text = "You win! Game created by Heather Parrett";
+            winSource.Play();
+        }
+
+        if (count == 4)
+        {
+            transform.position = new Vector2(0.0f, 41.5f);
+            life = 3;
+            lifeText.text = "<b> Lives: </b>" + life.ToString();
         }
     }
 }
